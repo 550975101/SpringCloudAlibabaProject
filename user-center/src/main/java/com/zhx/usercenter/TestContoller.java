160eefcaaf29eaded6aed1cf6a2c44b31cf43842
550975101@qq.com <550975101@qq.com>
2020-05-18 22:24:15 +0800

    用户中心整合通用mapper

---
+++ user-center/src/main/java/com/zhx/usercenter/TestContoller.java
@@ -0,0 +1,31 @@
+package com.zhx.usercenter;
+
+import com.zhx.usercenter.dao.user.UserMapper;
+import com.zhx.usercenter.domain.entity.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+/**
+ * 测试mybatis是否好使
+ */
+@RestController
+public class TestContoller {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @GetMapping("/test")
+    public User testInsert() {
+        User user = new User();
+        user.setAvatarUrl("xxxxx");
+        user.setBonus(100);
+        user.setCreateTime(new Date());
+        user.setUpdateTime(new Date());
+        this.userMapper.insertSelective(user);
+        return user;
+    }
+
+}
